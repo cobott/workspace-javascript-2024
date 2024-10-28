ff2393e3d64d7a11fb4e04c06b994a11d46331de
(cobott) <(coen.a.bott@gmail.com)>
2024-10-28 13:15:30 -0400

    10/28

--- js/arith.js
+++ js/arith.js
@@ -1,12 +1,16 @@
 // get the element 
-
+let spanE = document.getElementById('num1');
+let spanA = document.getElementById('num2');
 // element --- property
-var num1 = parseInt(document.getElementById('n1').textContent); 
+let n1 = parseInt(spanE.textContent);
+let n2 = parseInt(spanA.textContent);
+console.log(typeof n1);
+// var num1 = parseInt(document.getElementById('n1').textContent); 
 
 
 
 //process
-var c = num1 + num2;
+ var c = n1 + n2;
 
 //display
 document.write(c);
